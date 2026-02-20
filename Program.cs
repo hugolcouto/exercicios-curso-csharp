d56b51a00a333eaa6b9cc735fcb485906e5bdbd7
hugolcouto <contato@hugocouto.dev>
2026-02-19 21:53:18 -0300

    Pratica de FileStream e StreamReader

--- Program.cs
+++ Program.cs
@@ -1,8 +1,34 @@
-﻿class Program
+﻿using System.Xml;
+using DotNetEnv;
+
+class Program
 {
     public static void Main(string[] args)
     {
-        // Comece por aqui
-        Console.WriteLine("Hello World!");
+        Env.Load();
+
+        string path = Path.Combine(Environment.GetEnvironmentVariable("DIR_PATH")!, "file1.txt");
+
+        StreamReader sr = null!;
+
+        try
+        {
+            sr = File.OpenText(path);
+
+            while (!sr.EndOfStream)
+            {
+                string line = sr.ReadLine()!;
+                Console.WriteLine(line);
+            }
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine($"An error occurred: {e.Message}");
+            throw;
+        }
+        finally
+        {
+            if (sr != null) sr.Close();
+        }
     }
 }
--- obj/Debug/net10.0/curso-csharp.AssemblyInfo.cs
+++ obj/Debug/net10.0/curso-csharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("curso-csharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+949e64301848732d667a0ebe67e6b6a257dceb4f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c867b47bb061fb2356c581fa8ab3ba7df0c2e20d")]
 [assembly: System.Reflection.AssemblyProductAttribute("curso-csharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("curso-csharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
