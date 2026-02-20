519c47c4297cf10d6212ac9fd5845d8c97c4ffb1
hugolcouto <contato@hugocouto.dev>
2026-02-19 22:35:53 -0300

    pratica StreamWriter

--- Program.cs
+++ Program.cs
@@ -1,8 +1,30 @@
-﻿class Program
+﻿using DotNetEnv;
+
+class Program
 {
     public static void Main(string[] args)
     {
-        // Comece por aqui
-        Console.WriteLine("Hello World!");
+        Env.Load();
+
+        string sourcePath = Path.Combine(Environment.GetEnvironmentVariable("DIR_PATH")!, "file1.txt");
+        string targetPath = Path.Combine(Environment.GetEnvironmentVariable("DIR_PATH")!, "file2.txt");
+
+        try 
+        {
+            string[] lines = File.ReadAllLines(sourcePath);
+
+            using (StreamWriter sw = File.AppendText(targetPath))
+            {
+                foreach (string line in lines)
+                {
+                    sw.WriteLine(line.ToUpper());
+                }
+            }
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine($"An error occurred: {e.Message}");
+            throw;
+        }
     }
 }
