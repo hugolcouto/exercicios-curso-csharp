8d5f40651892d49ff02e534fea0e4bb42296e034
hugolcouto <contato@hugocouto.dev>
2026-02-19 22:59:28 -0300

    pratica Directory

--- Program.cs
+++ Program.cs
@@ -1,8 +1,34 @@
-﻿class Program
+﻿using DotNetEnv;
+
+class Program
 {
     public static void Main(string[] args)
     {
-        // Comece por aqui
-        Console.WriteLine("Hello World!");
+        Env.Load();
+
+        string path = Path.Combine(Environment.GetEnvironmentVariable("DIR_PATH")!, "myFolder");
+
+        try
+        {
+
+            IEnumerable<string> folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+            foreach (string folder in folders)
+            {
+                Console.WriteLine(folder);
+            }
+
+            IEnumerable<string> files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+            foreach (string file in files)
+            {
+                Console.WriteLine(file);
+            }
+
+            Directory.CreateDirectory(Path.Combine(path, "new_directory"));
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine($"An error occurred: {e.Message}");
+            throw;
+        }
     }
 }
