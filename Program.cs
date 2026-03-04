0dae09c983824f54ca6f36d3f8eb73ebad16b07d
hugolcouto <contato@hugocouto.dev>
2026-03-03 21:28:23 -0300

    Exercicio resolvido

--- Program.cs
+++ Program.cs
@@ -1,68 +1,36 @@
 ﻿using System.Runtime.CompilerServices;
+using curso_csharp.Entities;
+using DotNetEnv;
 
 class Program
 {
     public static void Main(string[] args)
     {
-        // string a = "Lorem";
-        // string b = "ipsum";
-        // string c = "Lorem";
-        // string d = a;
+        Env.Load();
 
-        // System.Console.WriteLine(a.Equals(b));
-        // System.Console.WriteLine(a.Equals(c));
-        // System.Console.WriteLine(a.GetHashCode());
-        // System.Console.WriteLine(b.GetHashCode());
-        // System.Console.WriteLine(c.GetHashCode());
-        // System.Console.WriteLine(d.GetHashCode());
+        string filePath = Path.Combine($"{Environment.GetEnvironmentVariable("DIR_PATH")!}/Data", "log.txt");
 
-        System.Console.WriteLine("---------------");
+        HashSet<LogRecord> set = new HashSet<LogRecord>();
 
-        HashSet<string> set = new HashSet<string>();
 
-        set.Add("Hello");
-        set.Add("world");
-        set.Add("from");
-        set.Add("CSharp!");
-
-        System.Console.WriteLine(set.Contains("CSharp!"));
-        System.Console.WriteLine(set.Contains("Javascript!"));
-
-        foreach (string item in set)
+        try
         {
-            System.Console.WriteLine(item);
+            using (StreamReader sr = File.OpenText(filePath))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] line = sr.ReadLine()!.Split(' ');
+                    string name = line[0];
+                    DateTime instant = DateTime.Parse(line[1]);
+                    set.Add(new LogRecord { UserName = name, Instant = instant });
+                }
+
+                Console.WriteLine($"Log file path: {filePath}\nTotal users: {set.Count}");
+            }
         }
-
-        System.Console.WriteLine("---------------");
-
-        SortedSet<int> a = new SortedSet<int>() { 10, 0, 2, 4, 6, 8 };
-        SortedSet<int> b = new SortedSet<int>() { 5, 6, 7, 8, 9, 10 };
-
-
-        SortedSet<int> ab = new SortedSet<int>(a);
-        ab.UnionWith(b);
-
-        PrintCollection(ab);
-
-        System.Console.WriteLine("---------------");
-
-        SortedSet<int> d = new SortedSet<int>(a);
-        d.IntersectWith(b);
-
-        PrintCollection(d);
-
-        System.Console.WriteLine("---------------");
-
-        SortedSet<int> e = new SortedSet<int>(a);
-        a.ExceptWith(b);
-        PrintCollection(e);
-    }
-
-    static void PrintCollection<T>(IEnumerable<T> collection)
-    {
-        foreach (T item in collection)
+        catch (IOException e)
         {
-            System.Console.WriteLine($"obj: {item}");
+            Console.WriteLine(e.Message);
         }
     }
 }
