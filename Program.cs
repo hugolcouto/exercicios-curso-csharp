b391480653ecc59ed2c461f5ad30bdc71c91b854
hugolcouto <contato@hugocouto.dev>
2026-03-04 21:40:07 -0300

    Pratica de dictionary

--- Program.cs
+++ Program.cs
@@ -2,7 +2,21 @@
 {
     public static void Main(string[] args)
     {
-        // Comece por aqui
-        Console.WriteLine("Hello World!");
+        Dictionary<string, string> cookies = new Dictionary<string, string>();
+
+        cookies["user"] = "Couto";
+        cookies["email"] = "couto@email.com";
+        cookies["phone"] = "(11)998765432";
+
+        cookies["user"] = "Hugo";
+        cookies["email"] = "hugo@email.com";
+        cookies["phone"] = "(11)912345678";
+
+
+
+        foreach (KeyValuePair<string, string> item in cookies)
+        {
+            System.Console.WriteLine($"{item.Key} => {item.Value}");
+        }
     }
 }
